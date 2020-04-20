23663c25c34da794a9fb245d69c4f6af90baa324
Girish Nair3 <GirishN3@citiustech.com>
2020-04-20 20:12:30 +0530

    added code

--- src/test/java/com/tests/AmazonTest.java
+++ src/test/java/com/tests/AmazonTest.java
@@ -2,59 +2,90 @@ package com.tests;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AmazonTest {
-	
-	WebDriver driver;
-	FileReader fr;
-	
+
+	public static WebDriver driver;
+	public static FileReader fr;
+	public static Object obj;
+	public static JSONParser jp;
+
 	@BeforeMethod
 	public void setUp() {
-		
+
 		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.in");
 		driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.MINUTES);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 	}
-	
-	
 
-	
-	
-	
-	@DataProvider(name="getData")
-	public void readJson() {
-		JSONParser jp= new JSONParser();
+	@Test()
+	public void searchProduct() {
+
+		driver.findElement(By.id("twotabsearchtextbox")).isDisplayed();
+	}
+
+	@DataProvider(name = "getData")
+	public String[] readJson() {
+		jp = new JSONParser();
+
 		try {
-			fr= new FileReader(".\\readJSON\\src\\test\\resources\\com\\data\\AmazonData.json");
-		} 
-		
+			fr = new FileReader(System.getProperty("user.dir") + "\\src\\test\\resources\\com\\data\\AmazonData.json");
+		}
+
 		catch (FileNotFoundException e) {
-			
+
 			e.printStackTrace();
 		}
-		
-		
-		Object obj = jp.parse(fr);
-		
+
+		try {
+			obj = jp.parse(fr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		JSONObject jobj = (JSONObject) obj;
+		JSONArray array = (JSONArray) jobj.get("AmazonProducts");
+		String arr[] = new String[array.size()];
+
+		for (int i = 0; i < array.size(); i++) {
+
+			JSONObject prod = (JSONObject) array.get(i);
+			String searchname = (String) prod.get("product");
+			System.out.println(searchname);
+			arr[i] = searchname;
+		}
+
+		return arr;
+
 	}
-	
-	
-	@AfterMethod
+
+	//@AfterMethod
 	public void tearDown() {
-		
-		
+
 		driver.quit();
 	}
 
